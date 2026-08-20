b123b024e2f3812b12c767a6e9f5b6a5139bf167
Bobby-21 <frbobby21@gmail.com>
2026-08-20 23:41:12 +0200

    update submits

--- condor/executable_step1.sh
+++ condor/executable_step1.sh
@@ -3,9 +3,10 @@ INPUT_DIR=$1
 OUTPUT_DIR=$2
 TRIGGER=$3
 DOGLOBAL=$4
+STANDARDIZE=$5
 
 echo "Setting up environment..."
 source $WORK_DIR/venv/bin/activate
 echo "Python: $(which python)"
 
-python $SCRIPT --input-dir $INPUT_DIR --output-dir $OUTPUT_DIR --layer-i 0 --layer-j 1 --layer-k 2 --trigger $TRIGGER --do-light 1 --use-best 1 $DOGLOBAL
+python $SCRIPT --input-dir $INPUT_DIR --output-dir $OUTPUT_DIR --layer-i 0 --layer-j 1 --layer-k 2 --trigger $TRIGGER --do-light 1 --use-best 1 $DOGLOBAL $STANDARDIZE
