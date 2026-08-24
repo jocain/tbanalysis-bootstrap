fb14734a1929ebbb5ff3c4c7b45597922411ea0c
Joshua George O'Cain <joocain@lxplus961.cern.ch>
2026-08-24 10:55:08 +0200

    Some fixes

--- condor/executable_step1.sh
+++ condor/executable_step1.sh
@@ -7,5 +7,5 @@ DOGLOBAL=$4
 echo "Setting up environment..."
 source $WORK_DIR/venv/bin/activate
 echo "Python: $(which python)"
-echo "python3 $SCRIPT --input-dir $INPUT_DIR --output-dir $OUTPUT_DIR --layer-i 0 --layer-j 1 --layer-k 2 --trigger $TRIGGER --do-light 1 --use-best 1 $DOGLOBAL"
-python3 $SCRIPT --input-dir $INPUT_DIR --output-dir $OUTPUT_DIR --layer-i 0 --layer-j 1 --layer-k 2 --trigger $TRIGGER --do-light 1 --use-best 1 $DOGLOBAL
+echo "python3 $SCRIPT --input-dir $INPUT_DIR --output-dir $OUTPUT_DIR --layer-i 0 --layer-j 1 --layer-k 2 --trigger $TRIGGER --use-best 1 $DOGLOBAL"
+python3 $SCRIPT --input-dir $INPUT_DIR --output-dir $OUTPUT_DIR --layer-i 0 --layer-j 1 --layer-k 2 --trigger $TRIGGER --use-best 1 $DOGLOBAL
--- run_step3.sh
+++ run_step3.sh
@@ -1,65 +1,65 @@
-python3 do_bootstrap_multiple_step2.py \
---input-dir results/Test-bootstrap_147124_147487_target-trigger-2_OPTSEL/step2/" \
+python3 do_bootstrap_multiple_step3.py \
+--input-dir "results/Test-bootstrap_147124_147487_target-trigger-2_OPTSEL/step2/" \
 --output-dir "results/Test-bootstrap_147124_147487_target-trigger-2_OPTSEL/step3/" \
 --run-start 147124
 
-python3 do_bootstrap_multiple_step2.py \
---input-dir results/Test-bootstrap_147488_147565_target-trigger-2_OPTSEL/step2/" \
+python3 do_bootstrap_multiple_step3.py \
+--input-dir "results/Test-bootstrap_147488_147565_target-trigger-2_OPTSEL/step2/" \
 --output-dir "results/Test-bootstrap_147488_147565_target-trigger-2_OPTSEL/step3/" \
 --run-start 147488
 
-python3 do_bootstrap_multiple_step2.py \
---input-dir results/Test-bootstrap_147566_147641_target-trigger-2_OPTSEL/step2/" \
+python3 do_bootstrap_multiple_step3.py \
+--input-dir "results/Test-bootstrap_147566_147641_target-trigger-2_OPTSEL/step2/" \
 --output-dir "results/Test-bootstrap_147566_147641_target-trigger-2_OPTSEL/step3/" \
 --run-start 147566
 
-python3 do_bootstrap_multiple_step2.py \
---input-dir results/Test-bootstrap_147644_147885_target-trigger-2_OPTSEL/step2/" \
+python3 do_bootstrap_multiple_step3.py \
+--input-dir "results/Test-bootstrap_147644_147885_target-trigger-2_OPTSEL/step2/" \
 --output-dir "results/Test-bootstrap_147644_147885_target-trigger-2_OPTSEL/step3/" \
 --run-start 147644
 
-python3 do_bootstrap_multiple_step2.py \
---input-dir results/Test-bootstrap_147886_147966_target-trigger-2_OPTSEL/step2/" \
+python3 do_bootstrap_multiple_step3.py \
+--input-dir "results/Test-bootstrap_147886_147966_target-trigger-2_OPTSEL/step2/" \
 --output-dir "results/Test-bootstrap_147886_147966_target-trigger-2_OPTSEL/step3/" \
 --run-start 147886
 
-python3 do_bootstrap_multiple_step2.py \
---input-dir results/Test-bootstrap_147967_148053_target-trigger-2_OPTSEL/step2/" \
+python3 do_bootstrap_multiple_step3.py \
+--input-dir "results/Test-bootstrap_147967_148053_target-trigger-2_OPTSEL/step2/" \
 --output-dir "results/Test-bootstrap_147967_148053_target-trigger-2_OPTSEL/step3/" \
 --run-start 147967
 
-python3 do_bootstrap_multiple_step2.py \
---input-dir results/Test-bootstrap_148847_148926_target-trigger-2_OPTSEL/step2/" \
+python3 do_bootstrap_multiple_step3.py \
+--input-dir "results/Test-bootstrap_148847_148926_target-trigger-2_OPTSEL/step2/" \
 --output-dir "results/Test-bootstrap_148847_148926_target-trigger-2_OPTSEL/step3/" \
 --run-start 148847
 
-python3 do_bootstrap_multiple_step2.py \
---input-dir results/Test-bootstrap_148927_149131_target-trigger-2_OPTSEL/step2/" \
+python3 do_bootstrap_multiple_step3.py \
+--input-dir "results/Test-bootstrap_148927_149131_target-trigger-2_OPTSEL/step2/" \
 --output-dir "results/Test-bootstrap_148927_149131_target-trigger-2_OPTSEL/step3/" \
 --run-start 148927
 
-python3 do_bootstrap_multiple_step2.py \
---input-dir results/Test-bootstrap_149132_149203_target-trigger-2_OPTSEL/step2/" \
+python3 do_bootstrap_multiple_step3.py \
+--input-dir "results/Test-bootstrap_149132_149203_target-trigger-2_OPTSEL/step2/" \
 --output-dir "results/Test-bootstrap_149132_149203_target-trigger-2_OPTSEL/step3/" \
 --run-start 149132 
 
-python3 do_bootstrap_multiple_step2.py \
---input-dir results/Test-bootstrap_149204_149279_target-trigger-2_OPTSEL/step2/" \
+python3 do_bootstrap_multiple_step3.py \
+--input-dir "results/Test-bootstrap_149204_149279_target-trigger-2_OPTSEL/step2/" \
 --output-dir "results/Test-bootstrap_149204_149279_target-trigger-2_OPTSEL/step3/" \
 --run-start 149204
 
-python3 do_bootstrap_multiple_step2.py \
---input-dir results/Test-bootstrap_149280_149428_target-trigger-2_OPTSEL/step2/" \
---output-dir "results/Test-bootstrap_149280_149428_target-trigger-2_OPTSEL/step3/" \
+python3 do_bootstrap_multiple_step3.py \
+--input-dir "results/Test-bootstrap_149280_149350_target-trigger-2_OPTSEL/step2/" \
+--output-dir "results/Test-bootstrap_149280_149350_target-trigger-2_OPTSEL/step3/" \
 --run-start 149280
 
-python3 do_bootstrap_multiple_step2.py \
---input-dir results/Test-bootstrap_149436_149584_target-trigger-2_OPTSEL/step2/" \
---output-dir "results/Test-bootstrap_149436_149584_target-trigger-2_OPTSEL/step3/" \
---run-start 149436
+#python3 do_bootstrap_multiple_step3.py \
+#--input-dir "results/Test-bootstrap_149436_149584_target-trigger-2_OPTSEL/step2/" \
+#--output-dir "results/Test-bootstrap_149436_149584_target-trigger-2_OPTSEL/step3/" \
+#--run-start 149436
 
-python3 do_bootstrap_multiple_step2.py \
---input-dir results/Test-bootstrap_149585_149665_target-trigger-2_OPTSEL/step2/" \
+python3 do_bootstrap_multiple_step3.py \
+--input-dir "results/Test-bootstrap_149585_149665_target-trigger-2_OPTSEL/step2/" \
 --output-dir "results/Test-bootstrap_149585_149665_target-trigger-2_OPTSEL/step3/" \
 --run-start 149585
 
