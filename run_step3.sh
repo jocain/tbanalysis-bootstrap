5cad38671aaf5c2456519186e6a76c36c98368a6
Joshua George O'Cain <joocain@lxplus922.cern.ch>
2026-09-03 17:28:53 +0200

    Restore run_step3,4.sh

--- run_step3.sh
+++ run_step3.sh
@@ -1,67 +1,9 @@
-export RESULTS_DIR="results_NPixStandardizedTWC_N5"
-
-python3 do_bootstrap_multiple_step3.py \
---input-dir "${RESULTS_DIR}/Test-bootstrap_147124_147487_target-trigger-2_OPTSEL/step2/" \
---output-dir "${RESULTS_DIR}/Test-bootstrap_147124_147487_target-trigger-2_OPTSEL/step3/" \
---run-start 147124
-
-python3 do_bootstrap_multiple_step3.py \
---input-dir "${RESULTS_DIR}/Test-bootstrap_147488_147565_target-trigger-2_OPTSEL/step2/" \
---output-dir "${RESULTS_DIR}/Test-bootstrap_147488_147565_target-trigger-2_OPTSEL/step3/" \
---run-start 147488
-
-python3 do_bootstrap_multiple_step3.py \
---input-dir "${RESULTS_DIR}/Test-bootstrap_147566_147641_target-trigger-2_OPTSEL/step2/" \
---output-dir "${RESULTS_DIR}/Test-bootstrap_147566_147641_target-trigger-2_OPTSEL/step3/" \
---run-start 147566
-
-python3 do_bootstrap_multiple_step3.py \
---input-dir "${RESULTS_DIR}/Test-bootstrap_147644_147885_target-trigger-2_OPTSEL/step2/" \
---output-dir "${RESULTS_DIR}/Test-bootstrap_147644_147885_target-trigger-2_OPTSEL/step3/" \
---run-start 147644
-
-python3 do_bootstrap_multiple_step3.py \
---input-dir "${RESULTS_DIR}/Test-bootstrap_147886_147966_target-trigger-2_OPTSEL/step2/" \
---output-dir "${RESULTS_DIR}/Test-bootstrap_147886_147966_target-trigger-2_OPTSEL/step3/" \
---run-start 147886
-
-python3 do_bootstrap_multiple_step3.py \
---input-dir "${RESULTS_DIR}/Test-bootstrap_147967_148053_target-trigger-2_OPTSEL/step2/" \
---output-dir "${RESULTS_DIR}/Test-bootstrap_147967_148053_target-trigger-2_OPTSEL/step3/" \
---run-start 147967
-
-python3 do_bootstrap_multiple_step3.py \
---input-dir "${RESULTS_DIR}/Test-bootstrap_148847_148926_target-trigger-2_OPTSEL/step2/" \
---output-dir "${RESULTS_DIR}/Test-bootstrap_148847_148926_target-trigger-2_OPTSEL/step3/" \
---run-start 148847
-
-python3 do_bootstrap_multiple_step3.py \
---input-dir "${RESULTS_DIR}/Test-bootstrap_148927_149131_target-trigger-2_OPTSEL/step2/" \
---output-dir "${RESULTS_DIR}/Test-bootstrap_148927_149131_target-trigger-2_OPTSEL/step3/" \
---run-start 148927
-
-python3 do_bootstrap_multiple_step3.py \
---input-dir "${RESULTS_DIR}/Test-bootstrap_149132_149203_target-trigger-2_OPTSEL/step2/" \
---output-dir "${RESULTS_DIR}/Test-bootstrap_149132_149203_target-trigger-2_OPTSEL/step3/" \
---run-start 149132 
-
-python3 do_bootstrap_multiple_step3.py \
---input-dir "${RESULTS_DIR}/Test-bootstrap_149204_149279_target-trigger-2_OPTSEL/step2/" \
---output-dir "${RESULTS_DIR}/Test-bootstrap_149204_149279_target-trigger-2_OPTSEL/step3/" \
---run-start 149204
-
-python3 do_bootstrap_multiple_step3.py \
---input-dir "${RESULTS_DIR}/Test-bootstrap_149280_149350_target-trigger-2_OPTSEL/step2/" \
---output-dir "${RESULTS_DIR}/Test-bootstrap_149280_149350_target-trigger-2_OPTSEL/step3/" \
---run-start 149280
-
-#python3 do_bootstrap_multiple_step3.py \
-#--input-dir "${RESULTS_DIR}/Test-bootstrap_149436_149584_target-trigger-2_OPTSEL/step2/" \
-#--output-dir "${RESULTS_DIR}/Test-bootstrap_149436_149584_target-trigger-2_OPTSEL/step3/" \
-#--run-start 149436
-
-python3 do_bootstrap_multiple_step3.py \
---input-dir "${RESULTS_DIR}/Test-bootstrap_149585_149665_target-trigger-2_OPTSEL/step2/" \
---output-dir "${RESULTS_DIR}/Test-bootstrap_149585_149665_target-trigger-2_OPTSEL/step3/" \
---run-start 149585
-
+#/eos/user/f/fernance/ETL/Bootstrap/April2026/Results_LeoCuts/ResultsFinalLineal_135958_136884
+#python do_bootstrap_multiple_step3.py --input-dir /eos/user/f/fernance/ETL/Bootstrap/July2026/Results/ResultsTWCUpdated_v2_147124_147487/step2 --output-dir /eos/user/f/fernance/ETL/Bootstrap/July2026/Results/ResultsTWCUpdated_v2_147124_147487/step3 --run-start 147124
+#python do_bootstrap_multiple_step3.py --input-dir /eos/user/f/fernance/ETL/Bootstrap/July2026/Results/ResultsTWCUpdated_v2_147488_147565/step2 --output-dir /eos/user/f/fernance/ETL/Bootstrap/July2026/Results/ResultsTWCUpdated_v2_147488_147565/step3 --run-start 147488
+#python do_bootstrap_multiple_step3.py --input-dir /eos/user/f/fernance/ETL/Bootstrap/July2026/Results/ResultsTWCUpdated_v2_147566_147641/step2 --output-dir /eos/user/f/fernance/ETL/Bootstrap/July2026/Results/ResultsTWCUpdated_v2_147566_147641/step3 --run-start 147566
+#python do_bootstrap_multiple_step3.py --input-dir /eos/user/f/fernance/ETL/Bootstrap/July2026/Results/ResultsTWCUpdated_v2_147644_147885/step2 --output-dir /eos/user/f/fernance/ETL/Bootstrap/July2026/Results/ResultsTWCUpdated_v2_147644_147885/step3 --run-start 147644
+#python do_bootstrap_multiple_step3.py --input-dir /eos/user/f/fernance/ETL/Bootstrap/July2026/Results/ResultsTWCUpdated_v2_147967_148053/step2 --output-dir /eos/user/f/fernance/ETL/Bootstrap/July2026/Results/ResultsTWCUpdated_v2_147967_148053/step3 --run-start 147967
+#python do_bootstrap_multiple_step3.py --input-dir /eos/user/f/fernance/ETL/Bootstrap/July2026/Results/ResultsTWCUpdated_v2_147886_147966/step2 --output-dir /eos/user/f/fernance/ETL/Bootstrap/July2026/Results/ResultsTWCUpdated_v2_147886_147966/step3 --run-start 147886
+python do_bootstrap_multiple_step3.py --input-dir /eos/user/f/fernance/ETL/Bootstrap/July2026/Results/ResultsTWCUpdated_v2_148847_148926/step2 --output-dir /eos/user/f/fernance/ETL/Bootstrap/July2026/Results/ResultsTWCUpdated_v2_148847_148926/step3 --run-start 148847
+#python do_bootstrap_multiple_step3.py --input-dir  --output-dir  --run-start
